a42ca47282128da9d5c2883c5b8b4a8566250bbb
Adrian L. Shaw <adrianlshaw@gmail.com>
2021-08-16 20:48:10 +0100

    Fix formatting issue

--- eth-price-check.py
+++ eth-price-check.py
@@ -3,51 +3,54 @@ import requests
 import os
 import sys
 
+
 def check_price(ticker):
 
-	# Get the ETH price 
+    # Get the ETH price
 
-        if ticket != "btc":
-	    eth_price_url = 'https://www.worldcoinindex.com/apiservice/ticker?key=' + api_key + '&label=' + ticker + 'btc&fiat=usd'  
-	    data = requests.get(eth_price_url).json()
-	    print(str(data))
-	    price_in_usd = data['Markets'][0]['Price']
-        else:
-            bitcoin_price_url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-            data = requests.get(bitcoin_price_url).json()
-            price_in_usd = data['bpi']['USD']['rate']
+    if ticket != "btc":
+        eth_price_url = 'https://www.worldcoinindex.com/apiservice/ticker?key=' + \
+            api_key + '&label=' + ticker + 'btc&fiat=usd'
+        data = requests.get(eth_price_url).json()
+        print(str(data))
+        price_in_usd = data['Markets'][0]['Price']
+    else:
+        bitcoin_price_url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+        data = requests.get(bitcoin_price_url).json()
+        price_in_usd = data['bpi']['USD']['rate']
 
-	# Post the message to the Discord webhook
-	data = {
-                "content": str(ticker.upper()) + " price is $" + str(price_in_usd) + " USD!"  
-	}
+    # Post the message to the Discord webhook
+    data = {
+        "content": str(ticker.upper()) + " price is $" + str(price_in_usd) + " USD!"
+    }
 
-	price_in_usd = str(price_in_usd).replace(',','')
-	
-	filename = "ath." + ticker
+    price_in_usd = str(price_in_usd).replace(',', '')
 
-	if os.path.isfile(filename) == False:
-	    f = open(filename, "a")
-	    f.write("0")
-	    f.close()
+    filename = "ath." + ticker
 
-	f = open(filename, "r+")
+    if os.path.isfile(filename) == False:
+        f = open(filename, "a")
+        f.write("0")
+        f.close()
 
-	ath = f.read()
+    f = open(filename, "r+")
 
-	if ath == "":
-	    ath = "0"
+    ath = f.read()
 
-	if float(price_in_usd) > float(ath):
-		print("ATH!!!")
-		requests.post(discord_webhook_url, data=data)
-		f.seek(0)
-		f.write(price_in_usd)
-	else:
-		print("Not an all time high")
-		print("ATH recorded at " + str(ath))
+    if ath == "":
+        ath = "0"
 
-	f.close()
+    if float(price_in_usd) > float(ath):
+        print("ATH!!!")
+        requests.post(discord_webhook_url, data=data)
+        f.seek(0)
+        f.write(price_in_usd)
+    else:
+        print("Not an all time high")
+        print("ATH recorded at " + str(ath))
+
+    f.close()
+
 
 # Provide the webhook URL that Discord generated
 discord_webhook_url = os.getenv('DISCORD_SECRET')
